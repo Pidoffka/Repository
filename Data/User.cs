a410876d450e89d56bf2e28e1768d36a71c17a53
YAMAHA RUKAHA <dan1axgod@gmail.com>
2019-12-15 19:46:55 +0300

    Класс User создан для хранения информации о пользователе.

---
+++ Data/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class User
+{
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string Login { get; set; }
+    public int Level { get; set; }
+    public int Money { get; set; }
+}
