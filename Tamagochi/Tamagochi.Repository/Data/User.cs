78eec7416981e9fc13d409198b6d14f6284a4815
YAMAHA RUKAHA <dan1axgod@gmail.com>
2019-12-15 20:44:37 +0300

    Добавлены параметры пользователя

--- Tamagochi/Tamagochi.Repository/Data/User.cs
+++ Tamagochi/Tamagochi.Repository/Data/User.cs
@@ -6,5 +6,10 @@ namespace Tamagochi.Repository.Data
 {
     class User
     {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public int Level { get; set; }
+        public int Money { get; set; }
     }
 }
