bc5be8ef806d9185ed59ad75174aae05c3e0d1bb
YAMAHA RUKAHA <dan1axgod@gmail.com>
2019-12-16 20:40:26 +0300

    Обновлен Repository ( Serialize и Deserialize написаны) Добавлен класс Item для товаров в магазине тамагоча

--- Tamagochi/Tamagochi.Repository/Repository.cs
+++ Tamagochi/Tamagochi.Repository/Repository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 using Tamagochi.Repository.Data;
 
@@ -9,14 +12,33 @@ namespace Tamagochi.Repository
     {
         public IEnumerable<User> Users => users;
         private List<User> users;
+        public IEnumerable<Item> Items => items;
+        private List<Item> items;
         public Repository()
         {
-
+            LoadData();
         }
-        private const string userfilename = "..//";
+        private const string usersfilename = "../../../../Tamagochi.Repository/Data/DataJson/Users.json";
+        private const string itemsfilename = "../../../../Tamagochi.Repository/Data/DataJson/Items.json";
+
         private void LoadData()
         {
-
+            users = JsonConvert.DeserializeObject<List<User>>(usersfilename);
+            items = JsonConvert.DeserializeObject<List<Item>>(itemsfilename);
         }
+
+        public void SaveData()
+        {
+            Serialize(items, itemsfilename);
+            Serialize(users, usersfilename);
+        }
+        private void Serialize<T>(T obj, string path)
+        {
+            string str = JsonConvert.SerializeObject(obj);
+            FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+            streamWriter.Write(str);
+        }
+
     }
 }
